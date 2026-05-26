003a015ef4d51399fb9466ccac1fe146aedb5db4
themeg25 <mhariprasad2001@gmail.com>
2026-05-26 15:29:07 +0530

    my first recat project

---
+++ App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="login-box">
+        <h2>Login Page</h2>
+        <p>Welcome Back</p>
+
+        <input type="text" placeholder="Enter Username" />
+
+        <input type="password" placeholder="Enter Password" />
+
+        <button>Login</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
